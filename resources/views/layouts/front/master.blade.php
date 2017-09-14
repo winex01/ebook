d7f32378c67eef9029ed853dec735c179b90ac51
winnie <winnie131212592@gmail.com>
2017-09-15 00:23:21 +0800

    WIP front template

--- resources/views/layouts/front/master.blade.php
+++ resources/views/layouts/front/master.blade.php
@@ -41,11 +41,11 @@
                 <hr />
                 <div class="row">
                       <div class="col-lg-3 col-md-3">
-                      <div class="panel panel-default">
-                          <div class="panel-body">
-                            <img src="{{ url('uploads/pages/sample1.jpg') }}" height="175px;" width="150px;">
+                         <div class="panel panel-default">
+                            <div class="panel-body">
+                              <img src="{{ url('uploads/pages/sample1.jpg') }}" height="175px;" width="150px;">
+                            </div>
                           </div>
-                        </div>
                       </div>
                       <div class="col-lg-9 col-md-9">
                        <h4><a href="#">Electronic Testing System</a></h4>
@@ -54,15 +54,21 @@
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum 
+                        <br />
+                        <div style=" margin-top: 5px;">
+                          <a href="#" class="btn btn-info btn-xs">Read more
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          </a>
+                        </div>
                       </div>
                 </div>
-                <hr />
 
                 <hr />
+
                 <div class="row">
                       <div class="col-lg-3 col-md-3">
-                      <div class="panel panel-default">
+                        <div class="panel panel-default">
                           <div class="panel-body">
                             <img src="{{ url('uploads/pages/sample1-2.jpg') }}" height="175px;" width="150px;">
                           </div>
@@ -79,8 +85,15 @@
                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        <br />
+                        <div style=" margin-top: 5px;">
+                          <a href="#" class="btn btn-info btn-xs">Read more
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          </a>
+                        </div>
                       </div>
                 </div>
+
                 <hr />
 
                 <nav class="col-md-offset-5" aria-label="Page navigation">

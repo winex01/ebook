ad978fe1a04e3bb7e54ebb9104d8db46743a84c6
winnie damayo <winnie131212592@gmail.com>
2017-10-29 20:38:42 +0800

    edit footer allright reserved

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,4 +2,4 @@
 {{-- <div class="pull-right hidden-xs">
 	<b>Version</b> 2.3.8
 </div> --}}
-<strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+<strong>Copyright &copy; 2014-2016 <a href="#">Palompon Institute of Technology I.C.T</a>.</strong> All rights reserved.

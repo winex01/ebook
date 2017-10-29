36683a164b57742d46276226d0aeac7cb01468c9
winnie damayo <winnie131212592@gmail.com>
2017-10-29 17:35:13 +0800

    template user profile icon

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -4,7 +4,8 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="{{ url('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+          {{-- <img src="{{ url('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image"> --}}
+          <img src="{{ url('images/administrator-icon.png') }}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>Administrator</p>
--- resources/views/layouts/user/sidebar.blade.php
+++ resources/views/layouts/user/sidebar.blade.php
@@ -4,10 +4,10 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="{{ url('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+          <img src="{{ url('images/user-icon.png') }}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Administrator</p>
+          <p>{{ ucwords(Auth::user()->name) }}</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>

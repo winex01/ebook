7316962df7766171d815f42e33e102e2cfc539fc
winnie damayo <winnie131212592@gmail.com>
2017-11-05 23:56:50 +0800

    add isset to book in index.blade.php modal

---
+++ resources/views/layouts/guest/mostViewed.blade.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <h4>Most Viewed</h4>
+    <ol>
+        <li><a href="#"> Arduino</a></li>
+        <li><a href="#"> Web</a></li>
+        <li><a href="#"> Management</a></li>
+        <li><a href="#"> SQL</a></li>
+        <li><a href="#"> Visual Studio</a></li>
+        <li><a href="#"> Windows Form</a></li>
+        <li><a href="#"> C#</a></li>
+        <li><a href="#"> Java</a></li>
+        <li><a href="#"> Game Dev</a></li>
+        <li><a href="#"> Networking</a></li>
+    </ol>
+</div>
